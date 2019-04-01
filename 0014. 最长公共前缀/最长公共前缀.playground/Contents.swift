23ba233fe7925b34fc643e5a9e9accedd58db0e0
ethanz <ethanz1990@163.com>
2019-04-01 19:39:39 +0800

    修改文件名

---
+++ 0014. 最长公共前缀/最长公共前缀.playground/Contents.swift
@@ -0,0 +1,47 @@
+import UIKit
+
+/*
+ 编写一个函数来查找字符串数组中的最长公共前缀。
+ 
+ 如果不存在公共前缀，返回空字符串 ""。
+ 
+ 示例 1:
+ 
+ 输入: ["flower","flow","flight"]
+ 输出: "fl"
+ 
+ 示例 2:
+ 
+ 输入: ["dog","racecar","car"]
+ 输出: ""
+ 解释: 输入不存在公共前缀。
+ 
+ 说明:
+ 
+ 所有输入只包含小写字母 a-z 。
+
+ */
+
+
+
+func longestCommonPrefix(_ strs: [String]) -> String {
+    
+    let firstStr =  strs.first!
+    
+    var prefix = ""
+    
+    for (index, char) in firstStr.enumerated(){
+    
+        for(index2, str) in str
+        
+        
+        
+    }
+    
+    
+    
+    
+    
+    
+    return ""
+}
